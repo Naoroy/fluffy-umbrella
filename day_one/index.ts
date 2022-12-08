5a3438c9eecd8ec0171875ae86e900f4de7dda96
Fégan <26162328+Naoroy@users.noreply.github.com>
2022-12-08 12:12:55 +0100

    wip

---
+++ day_one/index.ts
@@ -0,0 +1,13 @@
+function main() {
+
+}
+
+function getCalories() {
+
+}
+
+type Food = { name: string, calories: number }
+type getElves = (list: string) => Food[]
+const getElves: getElves = (list) => {
+  return []
+}
